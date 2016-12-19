c1dc0e59596eb8a2e4472f526e5493a65f4c809b
weras <weras11@gmail.com>
2016-12-19 02:15:47 +0200

    Modified the echo test to not actually fail... The file is a mess due to lots of experimentation, that often require custom solutions.

--- run_echo_tests.m
+++ run_echo_tests.m
@@ -12,11 +12,14 @@ function [ results, failed ] = run_echo_tests()
                 '!@#$%^&*()_+', ...
                 'אטזכבנרû‏', ...
                 ' g h j y  2    a    f   '};
+% 
+%   watermarks = {'Tekstas uzslepimui'};
 
 %   audio_names = {'1_mono', '66', '69', '70'};
 
   % NOTE: 1 and 2 are mono; 69, 69 and 70 are stereo
-  audio_names = [1, 66, 69, 70];
+%  audio_names = [1, 66, 69, 70, 51];
+  audio_names = [51];
 
   % zero delay = 0.61 | Should result in 27 samples
   % one delay = 0.73  | Should result in 32 samples
@@ -24,10 +27,16 @@ function [ results, failed ] = run_echo_tests()
   zero_delay = 0.61;
   one_delay = 0.73;
   decay_rate = 0.8;
+  
+%   zero_delays = [0.1, 0.3, 0.5];
+%   one_delays = 0.73;
+%   decay_rates = 0.8;
+  
 
   global algorithm_id;
   algorithm_id = 1;
-  
+
+%   for
   for audio_name = audio_names
     results(audio_name) = [];
     failed(audio_name) = 0;
@@ -39,18 +48,24 @@ function [ results, failed ] = run_echo_tests()
         [result,ODG] = AllDoDetection(audio_name, text, Nite, zero_delay, one_delay, decay_rate, random_stuff);
         ODG
 
-        if result.orig > 0
-          failed(audio_name) = failed(audio_name) + 1;
-        else
-          results(audio_name) = [results(audio_name) result];
+        if result.orig == 0
+            failed(audio_name) = failed(audio_name) + 1;
         end
+
+        results(audio_name) = [results(audio_name) result];
+
     end
       
   end
+%   end
 
   for k=cell2mat(keys(results))
       display(k);
       vals = results(k);
+      if isempty(vals)
+          display('Nothing to show equate to 0');
+          continue;
+      end
       fn = fieldnames(vals(1));
       for j = 1:length(fn)
         f = fn(j);
